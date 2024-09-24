d60da91d1650aef46523167ce8a96b6314453c1b
mohanish <mohanish.ghate@gmail.com>
2024-09-24 16:15:15 +0530

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,31 @@
+
+public class Hello {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		System.out.println("Welcome to Java !!");
+
+		int x = 10; // integer variable
+		int y = 20; // another integer variable
+
+		System.out.println(x);
+		System.out.println(y);
+
+		x = 50; // change the value in the variable
+		System.out.println(x);
+
+		float per1 = (float) 80.75; // float variable
+		double per2 = 93.5; // double variable
+		System.out.println(per1);
+		System.out.println(per2); // short cut -> syso Control + Space
+
+		char c = 'A'; // character variable
+		System.out.println(c);
+		System.out.println('A');
+
+		boolean validUser = true;
+		System.out.println(validUser);
+
+	}
+}
